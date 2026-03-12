41e63e370f6bdcda086532f8909e6af78ec4c3aa
blafleur91 <blafleur@byupathway.edu>
2026-03-11 22:04:49 -0600

    finished the team activity for week02

---
+++ week02/Journal/PromptGenerator.cs
@@ -0,0 +1,10 @@
+public class PromptGenerator
+{
+    public List<string> _prompts;
+
+    public string GetRandomPrompt()
+    {
+        // during the beginning stages of writing the program, you can do this to return an emprty sting
+        return "";
+    }
+}
